aaa7f5a051c6af18ba3aca9aabda370ce71d29d7
Arvin Fernandez <arvinf07@gmail.com>
2020-11-05 09:44:53 -0500

    commit tree 2b727d4ea6b8471fe2a49ab85920a1e250739e4a

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,51 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end  
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end  
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    new_song.save
+    new_song
+  end  
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end  
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end  
+
+  def self.new_from_filename(song)
+    song = song.tr('-.', '.')
+    song = song.split('.')
+    new_song = self.new_by_name(song[1].strip)
+    new_song.artist_name = song[0].strip
+    new_song
+  end 
+
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name).save
+  end 
+
+  def self.destroy_all
+    self.all.clear
+  end  
 
 end
